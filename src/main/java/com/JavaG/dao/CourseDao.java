cc523218e41897887632b649b66fcd04dcb626e2
zhangjingmin <1553316823@qq.com>
2020-12-18 12:06:34 +0800

    课程和老师的结合

--- src/main/java/com/JavaG/dao/CourseDao.java
+++ src/main/java/com/JavaG/dao/CourseDao.java
@@ -1,6 +1,7 @@
 package com.JavaG.dao;
 
 import com.JavaG.domain.Course;
+import com.JavaG.domain.SC;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,5 @@ public interface CourseDao {
     //通过学生id查找该学生课程
 
     List<Course> findAllCourseByStudentId(int id);
+    Boolean deleteStudentCourse(SC sc);
 }
--- src/main/java/com/JavaG/service/impl/CourseServiceImpl.java
+++ src/main/java/com/JavaG/service/impl/CourseServiceImpl.java
@@ -2,6 +2,7 @@ package com.JavaG.service.impl;
 
 import com.JavaG.dao.CourseDao;
 import com.JavaG.domain.Course;
+import com.JavaG.domain.SC;
 import com.JavaG.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,10 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public List<Course> findAllCourseByStudentId(int id) {
         return courseDao.findAllCourseByStudentId(id);
+    }
+
+    @Override
+    public Boolean deleteStudentCourse(SC sc) {
+        return courseDao.deleteStudentCourse(sc);
     }
 }
