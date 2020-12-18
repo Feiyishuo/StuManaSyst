9164a8f4408a597dd9dad789cef7fea3c756b764
WeiChangxin <850974330@qq.com>
2020-12-18 13:47:03 +0800

    登录、修改密码、退出登录

--- src/main/java/com/JavaG/domain/User.java
+++ src/main/java/com/JavaG/domain/User.java
@@ -1,13 +1,52 @@
 package com.JavaG.domain;
 
-import java.io.Serializable;
 
-public class User implements Serializable {
+//User实体类
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private int type=1;
+    private int status=1;
 
-    private String uid;   //用户名
-    private String userPwd;    //密码
-    private Integer userType;  //权限
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
+
